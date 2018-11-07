0f9734f04e2873a11c8dd49e1b8184554a70687e
Anders Lindén <purist.coder@gmail.com>
2018-11-07 13:24:40 +0100

    Updating .NET version from 3.5 to 4.7.2.

--- mssqldump/Program.cs
+++ mssqldump/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
-using System.Linq;
-using System.Text;
 using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlServer.Management.Sdk.Sfc;
 
